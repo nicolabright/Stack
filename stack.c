dbe2211ea86885c20c199d0b5355d6b0dccaaeee
nicolabright <nicola.camon@gmail.com>
2022-11-22 22:56:40 +0100

    Finalmente uno Stack decente...

--- stack.c
+++ stack.c
@@ -2,94 +2,67 @@
 #include <stdlib.h>
 
 typedef struct StackElement {
-	struct StackElement *topElement;
 	struct StackElement *bottomElement;
 	int value;
-	int elementUsed;
 } Stack;
 
-void StackInit(Stack **ptrStack) {
-	(*ptrStack) = malloc( sizeof(Stack)*1 );
-	(*ptrStack)->topElement = NULL;
-	(*ptrStack)->bottomElement = NULL;
-	(*ptrStack)->value = -1;
-	(*ptrStack)->elementUsed = -1;
-}
-
 void StackPush(Stack **ptrStack, int pushValue) {
-	if ((*ptrStack)->elementUsed != -1) {
-		(*ptrStack)->topElement = (Stack *) malloc( sizeof(Stack)*1);
-		(*ptrStack)->topElement->bottomElement = (*ptrStack);
-		(*ptrStack) = (*ptrStack)->topElement;
+	if ( (*ptrStack) == NULL ) {
+		(*ptrStack) = (Stack *) malloc( sizeof(Stack) );
+		(*ptrStack)->bottomElement = NULL;
+	} else {
+		Stack *next = (Stack *) malloc( sizeof(Stack) );
+		next->bottomElement = (*ptrStack);
+		(*ptrStack) = next;
 	}
-	(*ptrStack)->topElement = NULL;
 	(*ptrStack)->value = pushValue;
-	(*ptrStack)->elementUsed = 0;
 }
 
 int StackPop(Stack **ptrStack) {
-	if ((*ptrStack)->elementUsed == -1) {
-		return (0);
-	} else {
-		int returnValue = (*ptrStack)->value;
-		if ((*ptrStack)->bottomElement != NULL) {
-			(*ptrStack) = (*ptrStack)->bottomElement;
-			free((*ptrStack)->topElement);
-			(*ptrStack)->topElement = NULL;
-		} else {
-			(*ptrStack)->elementUsed = -1;
-		}
-		return (returnValue);
-	}
+	if ( (*ptrStack) == NULL ) return (0);
+	int returnValue = (*ptrStack)->value;
+	Stack *previous = (*ptrStack)->bottomElement;
+	free(ptrStack);
+	(*ptrStack) = previous;
+	return (returnValue);
 }
 
 void StackPrint(Stack **ptrStack) {
 	Stack *scanStack = (*ptrStack);
 	printf("-- Stack ----------------------------\n");
-	while ( (scanStack->bottomElement != NULL) ) {
+	while ( (scanStack != NULL) ) {
 		printf("[ %d\t ]\n", scanStack->value);
 		scanStack = scanStack->bottomElement;
-	};
-	if (scanStack->elementUsed == 0) {
-		printf("[ %d\t ]\n", scanStack->value);
 	}
 	printf("-------------------------------------\n");
 }
 
+
 void main() {
-	Stack *myStack;
-	StackInit(&myStack);
-	StackPrint(&myStack);
-
+	Stack *myStack = NULL;
 	StackPush(&myStack, 1);
-//	StackPrint(&myStack);
-
+	StackPrint(&myStack);
 	StackPush(&myStack, 2);
-	StackPush(&myStack, 3);
-	StackPush(&myStack, 4);
 	StackPrint(&myStack);
-
 	StackPush(&myStack, 3);
+	StackPrint(&myStack);
 	printf("Pop: %d\n", StackPop(&myStack));
+	printf("Pop: %d\n", StackPop(&myStack));
+	StackPrint(&myStack);
 	StackPush(&myStack, 4);
-	printf("Pop: %d\n", StackPop(&myStack));
 	StackPush(&myStack, 5);
-	printf("Pop: %d\n", StackPop(&myStack));
-	StackPrint(&myStack);
-	printf("Pop: %d\n", StackPop(&myStack));
-
-	printf("Pop: %d\n", StackPop(&myStack));
-	printf("Pop: %d\n", StackPop(&myStack));
-	printf("Pop: %d\n", StackPop(&myStack));
-	StackPrint(&myStack);
-	printf("Pop: %d\n", StackPop(&myStack));
+	StackPush(&myStack, 6);
+	StackPush(&myStack, 7);
 	StackPrint(&myStack);
 	printf("Pop: %d\n", StackPop(&myStack));
 	printf("Pop: %d\n", StackPop(&myStack));
 	printf("Pop: %d\n", StackPop(&myStack));
 	printf("Pop: %d\n", StackPop(&myStack));
 	printf("Pop: %d\n", StackPop(&myStack));
+	printf("Pop: %d\n", StackPop(&myStack));
+	printf("Pop: %d\n", StackPop(&myStack));
+	printf("Pop: %d\n", StackPop(&myStack));
+	printf("Pop: %d\n", StackPop(&myStack));
+	printf("Pop: %d\n", StackPop(&myStack));
 	StackPrint(&myStack);
-
-	printf("==============================================================\n");
 }
